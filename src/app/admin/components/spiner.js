8eb0b65b5a07efc5acdbbb9fa8bec6a693f24c10
Ajay Shrivas <ajay.shrivas@himanshusofttech.com>
2023-12-01 12:04:54 +0530

    Create Spinner
    We are create Spinner

---
+++ src/app/admin/components/spiner.js
@@ -0,0 +1,13 @@
+// Spinner.jsx
+import React from 'react';
+import styles from './Spinner.module.css';
+
+const Spinner = () => {
+  return (
+    <div className={styles.spinnerOverlay}>
+      <div className={styles.spinner}></div>
+    </div>
+  );
+};
+
+export default Spinner;
