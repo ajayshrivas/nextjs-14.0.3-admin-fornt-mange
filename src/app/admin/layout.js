b649ab43b8ffac5d233115b25917d2e7df321eb6
Ajay Shrivas <ajay.shrivas@himanshusofttech.com>
2023-12-01 12:03:53 +0530

    Loading UI and Streaming
    We are added Loading UI and Streaming <Suspense>

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,6 +1,7 @@
 import { Suspense } from 'react'
 import Topbar from './components/topbar';
 import Sidebar from './components/sidebar';
+import Loading from "./loading";
 export default function DashboardLayout({
   children, // will be a page or nested layout
 }) {
@@ -12,7 +13,7 @@ export default function DashboardLayout({
         <Sidebar/>
         {/* Include shared UI here e.g. a header or sidebar */}
         <div className="flex-1 p-4">
-          <Suspense fallback={<p>Loading feed...</p>}>
+          <Suspense fallback={<Loading />}>
           {children}
           </Suspense>
          </div>
--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,13 +1,39 @@
 
 import Link from "next/link";
+async function getMovies() {
+
+   // let res = await fetch(
+   //   'https://jsonplaceholder.typicode.com/posts'
+   // );
+   // console.log(res);
+   // await new Promise((resolve) => setTimeout(resolve, 2000));
+   // return res.json();
+   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+   await new Promise((resolve) => setTimeout(resolve, 2000));
+   const results = await res.json();
+   return results;
+ }
+ 
+ async function Trending() {
+      let results = await  getMovies();
+      //console.log(results);
+      return (
+         <div>
+            <h3>Movies</h3>
+            {results &&
+               results.map((index) => {
+               return <li>{index.title}</li>;
+               })}
+         </div>
+      );
+ }
+ 
 export default function Dashboard(){
    
-    return(
-    <>
-      
-       <h1>Dashboard Layout</h1>
-      
-   
-    </>
-   );
+   return(
+      <>
+      <h1>Dashboard Layout</h1>
+      <Trending/>
+      </>
+    );
 }
